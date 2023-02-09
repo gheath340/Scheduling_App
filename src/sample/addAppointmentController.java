10dac5558e5a8bca8d8a2450bed740c486954ddd
Garrett Heath <heathsmail0@gmail.com>
2023-02-09 09:46:50 -0700

    adding appointments onAddClick progress

--- src/sample/addAppointmentController.java
+++ src/sample/addAppointmentController.java
@@ -1,5 +1,6 @@
 package sample;
 
+import DBAccess.DBAppointments;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -11,6 +12,8 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.sql.Timestamp;
 
 public class addAppointmentController {
 
@@ -27,7 +30,23 @@ public class addAppointmentController {
     public ComboBox contactField;
 
     public void onAddClick(ActionEvent actionEvent) {
+        String title = titleField.getText();
+        String location = locationField.getText();
+        String description = descriptionField.getText();
+        String type = typeField.getText();
+        String startString = startField.getText();
+        Timestamp start = Timestamp.valueOf(startString);
+        String endString = endField.getText();
+        Timestamp end = Timestamp.valueOf(endString);
+        String customerID = customerIDField.getText();
+        String userID = userIDField.getText();
+        String contact = (String) contactField.getValue();
+        long millis = System.currentTimeMillis();
+        Date date = new java.sql.Date(millis);
+        String user = LoginController.getUser();
 
+        DBAppointments.appointmentAdd(title, description, location, type, date, user, date,
+                user, start, end, customerID, userID, contactID);
     }
 
     public void onExitClick(ActionEvent actionEvent) throws IOException {
